eee103c102244304e9722b6d7bb7f59be27127ba
Jasmine Ryon <jryon12@priorypride.net>
2021-06-21 14:01:36 -0700

    commit tree 1b4d92a42d85f30ded7f73c608ccfedb221dd74a

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+def new
+end
+  
+def create
+    @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to :welcome
+        else
+            return head(:forbidden)
+            redirect_to :login
+        end
+end
+  
+end
